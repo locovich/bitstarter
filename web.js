e0106499ddb48f3371650d85c715e2de949308e1
John Smith <example@stanford.edu>
2013-07-13 14:21:58 +0000

    Added fs.readFileSync and buffer.toString to web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var encoding = 'utf8';
 
 var app = express.createServer(express.logger());
 
+var indexBuffer = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexBuffer.toString(encoding));
 });
 
 var port = process.env.PORT || 5000;
